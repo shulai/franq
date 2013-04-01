8de0b4f4ffb67478160c726ca6ef8628d10f2c74
Julio Gazquez <julio@mebamutual.com.ar>
2013-04-01 11:14:19 -0300

    Add license (COPYING) file and license headers

--- setup.py
+++ setup.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the Franq reporting framework
+# Franq is (C)2012,2013 Julio César Gázquez
+#
+# Franq is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# any later version.
+#
+# Franq is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Franq; If not, see <http://www.gnu.org/licenses/>.
 
 from setuptools import setup
 
