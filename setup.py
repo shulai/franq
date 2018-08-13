f5f0a4c4c83f8a85b273339b34547b1ec6871556
Julio César Gázquez <julio@mebamutual.com.ar>
2018-08-13 11:17:49 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup
 
 setup(
     name='franq',
-    version='0.9.11',
+    version='0.9.13',
     description="PyQt-based reporting framework",
     author="Julio Cesar Gazquez",
     author_email='julio@mebamutual.com.ar',
